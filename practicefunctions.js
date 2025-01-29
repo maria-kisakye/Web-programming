a87612d0b56ecfd16925792e76f740fa13ffd2a4
maria-kisakye <kismar1120@gmail.com>
2025-01-29 17:48:36 +0300

    Add files via upload
    this includes practice of functions in javascript

---
+++ practicefunctions.js
@@ -0,0 +1,22 @@
+function multiplication(numberOne, numberTwo){
+    return numberOne * numberTwo;
+}
+console.log(multiplication(2, 5));
+
+
+function multiply(numberOne, numberTwo){
+    let product = numberOne * numberTwo;
+    console.log(product)
+}
+multiply(10, 5)
+
+
+const addition = (numberOne, numberTwo) =>{
+    return numberOne + numberTwo;
+}
+console.log(addition(2, 3))
+
+const welcome = (username) =>{
+    console.log(`welcome back ${username}!`);
+}
+welcome("Maria");
