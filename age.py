f442d676cee99efe8cb2f4387741ae8fe4ceca9a
maria-kisakye <kismar1120@gmail.com>
2025-02-08 12:10:10 +0300

    Add files via upload

---
+++ age.py
@@ -0,0 +1,6 @@
+from datetime import date
+
+date_of_birth = int(input("In which year you took birth"))
+
+today_date = date.today().strftime("%Y")
+print("your current age is ",int(today_date)-date_of_birth)
