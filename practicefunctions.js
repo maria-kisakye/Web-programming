2b0dd9e3b6378854da0c1a2c9a368d4e48dba485
maria-kisakye <kismar1120@gmail.com>
2025-02-10 12:58:31 +0300

    Add files via upload

--- practicefunctions.js
+++ practicefunctions.js
@@ -1,22 +1,47 @@
-function multiplication(numberOne, numberTwo){
-    return numberOne * numberTwo;
-}
-console.log(multiplication(2, 5));
+// function multiplication(numberOne, numberTwo){
+//     return numberOne * numberTwo;
+// }
+// console.log(multiplication(2, 5));
 
 
-function multiply(numberOne, numberTwo){
-    let product = numberOne * numberTwo;
-    console.log(product)
-}
-multiply(10, 5)
+// function multiply(numberOne, numberTwo){
+//     let product = numberOne * numberTwo;
+//     console.log(product)
+// }
+// multiply(10, 5)
 
 
-const addition = (numberOne, numberTwo) =>{
-    return numberOne + numberTwo;
-}
-console.log(addition(2, 3))
+// const addition = (numberOne, numberTwo) =>{
+//     return numberOne + numberTwo;
+// }
+// console.log(addition(2, 3))
 
-const welcome = (username) =>{
-    console.log(`welcome back ${username}!`);
-}
-welcome("Maria");
+// const welcome = (username) =>{
+//     console.log(`welcome back ${username}!`);
+// }
+// welcome("Maria");
+
+
+// function multiply(numOne, numTwo){
+//     return numOne*numTwo;
+// }
+// console.log(multiply(5, 6));
+
+
+// function multiplication(numOne, numTwo){
+//     let product = numOne*numTwo;
+//     console.log(product)
+// }
+// multiplication(10, 10)
+
+
+// let years = 10
+// if (years<18){
+//     console.log('minor')
+// }else if (years>=18){
+//     console.log("adult")
+// }else{
+//     console.log('invalid')
+// }
+
+
