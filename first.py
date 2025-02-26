790fb1b51be36624373f3be6bbce15154c312732
maria-kisakye <kismar1120@gmail.com>
2025-02-26 10:58:22 +0300

    Add files via upload

---
+++ first.py
@@ -0,0 +1,17 @@
+# print("Hello world")
+
+# number_one = 3
+# number_two = 6
+# print(number_one+number_two)
+
+# print(f"{number_one}+{number_two} = {number_one+number_two}")
+
+
+# Using break to exit the loop
+# for i in range(1, 11):
+#     if i == 6:
+#         break
+#     print(i)
+
+list = [1 ,2 ,3 ,4]
+print(list[::-1])
